82bd57954c1b9753e6b496688f9728cdc0952e01
4000D <dkdkajej@gmail.com>
2016-11-03 17:01:25 +0900

    export Editors to seven-main

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 export { default as RichTextEditor } from './src/RichTextEditor'
+export { EditorValue, decorator, createEmptyValue, createValueFromString } from './src/RichTextEditor'
+
 export { default as SimpleRichTextEditor } from './src/SimpleRichTextEditor'
